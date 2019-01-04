e2f16d6bd70c2e547a78700931ad64206bd07f6e
XiSHEN0220 <shenxiluc@gmail.com>
2019-01-03 20:19:55 -0500

    model

--- model/download_models.sh
+++ model/download_models.sh
@@ -1,3 +1,6 @@
 wget 'https://www.dropbox.com/s/riyzmksp9qma8hn/brueghelModel.pth?dl=0'
+mv brueghelModel.pth?dl=0 brueghelModel.pth
 wget 'https://www.dropbox.com/s/cqv60rlvefpt5mi/ltllModel.pth?dl=0'
+mv ltllModel.pth?dl=0 ltllModel.pth
 wget 'https://www.dropbox.com/s/z3c0m4c99b17lf3/resnet18.pth?dl=0'
+mv resnet18.pth?dl=0 resnet18.pth
