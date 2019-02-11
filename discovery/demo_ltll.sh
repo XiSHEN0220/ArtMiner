625d0ff16497b65f9ded7d8c122769764a8364b1
XiSHEN0220 <shenxiluc@gmail.com>
2019-02-11 13:09:24 -0500

    ltll retrieval

---
+++ discovery/demo_ltll.sh
@@ -0,0 +1,8 @@
+
+
+python dataset_discovery.py --computeSaliencyCoef --valOrTest test --outResJson out1.json --houghInitial --nbIter 1000
+python dataset_discovery.py --computeSaliencyCoef --valOrTest test --outResJson out2.json --finetunePath ../model/ltllModel.pth --houghInitial --nbIter 1000
+
+
+
+
