7b056e9f7bb4b2d9eb27dae9c632efb6c5504819
XiSHEN0220 <shenxiluc@gmail.com>
2019-02-11 13:10:05 -0500

    download

--- data/download_dataset.sh
+++ data/download_dataset.sh
@@ -5,3 +5,10 @@ rm Brueghel.zip?dl=0
 wget 'https://www.dropbox.com/s/tvrt7v7m5kf1ssw/Ltll.zip?dl=0'
 unzip Ltll.zip?dl=0
 rm Ltll.zip?dl=0
+
+wget 'http://www.robots.ox.ac.uk/~vgg/data/oxbuildings/oxbuild_images.tgz'
+mkdir Oxford5K
+mkdir Oxford5K/img
+tar xvf oxbuild_images.tgz -C Oxford5K/img
+rm oxbuild_images.tgz
+
