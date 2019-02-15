eae200dced9bb4c924b64682a9902924afd4650e
XiSHEN0220 <shenxiluc@gmail.com>
2019-02-15 09:45:03 +0100

    demo ltll

--- discovery/demo_ltll.sh
+++ discovery/demo_ltll.sh
@@ -1,7 +1,7 @@
 
 
-python dataset_discovery.py --computeSaliencyCoef --valOrTest test --outResJson out1.json --houghInitial --nbIter 1000
-python dataset_discovery.py --computeSaliencyCoef --valOrTest test --outResJson out2.json --finetunePath ../model/ltllModel.pth --houghInitial --nbIter 1000
+python dataset_discovery.py --computeSaliencyCoef --valOrTest test --outResJson out1.json --houghInitial --nbIter 1000 --minFeatCC 3 --finetunePath ../model/resnet18.pth
+python dataset_discovery.py --computeSaliencyCoef --valOrTest test --outResJson out2.json --finetunePath ../model/ltllModel.pth --houghInitial --nbIter 1000 --minFeatCC 3
 
 
 
