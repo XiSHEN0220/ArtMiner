22056224c30e247b04b01244b419fb16dd3522c3
XiSHEN0220 <shenxiluc@gmail.com>
2019-03-06 21:57:33 +0100

    oxford

---
+++ discovery/demo_oxford.sh
@@ -0,0 +1,3 @@
+python oxford_retrieval.py --scaleImgRef 25 --houghInitial --cuda --featScaleBase 20 --finetunePath ../model/resnet.pth --valOrTest test --outResJson Oxford_resnet.json --scaleImgRef 25
+
+python oxford_retrieval.py --scaleImgRef 25 --houghInitial --cuda --featScaleBase 20 --finetunePath ../model/oxford.pth --valOrTest test --outResJson Oxford_finetune.json --scaleImgRef 25
